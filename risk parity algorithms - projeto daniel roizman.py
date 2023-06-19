b2b2daa82ef2a5f315bff696562975e7b6c134a5
d-roizman <136944475+d-roizman@users.noreply.github.com>
2023-06-19 01:35:48 -0300

    Update risk parity algorithms - projeto daniel roizman.py

--- risk parity algorithms - projeto daniel roizman.py
+++ risk parity algorithms - projeto daniel roizman.py
@@ -9,6 +9,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pyfeng as pf
+import requests
+from bs4 import BeautifulSoup
+import yfinance as yf
 import time
 
 # ________________________________________________________________
